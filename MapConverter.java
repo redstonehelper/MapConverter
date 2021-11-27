64864074338f378e06b54286eed2029cd8052cbe
redstonehelper <76822346+redstonehelper@users.noreply.github.com>
2021-11-27 18:20:37 +0100

    Update for 1.17

--- MapConverter.java
+++ MapConverter.java
@@ -14,11 +14,11 @@ public class MapConverter {
 	public static final int VARIATION_DARKER = 0;
 	public static final int VARIATION_NORMAL = 1;
 	public static final int VARIATION_LIGHTER = 2;
-	private static final int numberOfBaseColors = 58;
+	private static final int numberOfBaseColors = 61;
 	public static final String BLOCK_STATE_GLASS = "minecraft:glass";
 	public static final String BLOCK_STATE_WATER = "minecraft:water[level=0]";
 
-	private static final String version = "0.0.16";
+	private static final String version = "0.0.17";
 	private static final String REDDIT_URL = "https://www.reddit.com/r/Minecraft/comments/gu1npm";
 
 	public static enum Mode {
@@ -292,13 +292,14 @@ public class MapConverter {
 	}
 
 	private static void initColorData() {
-		// Borrowed from bcy.class in 1.12-pre7.jar or cxe.class in 1.16.1
+		// Borrowed from bcy.class in 1.12-pre7.jar or cxe.class in 1.16.1 or dex.class
+		// in 1.17.1
 		int[] baseColors = { 8368696, 16247203, 13092807, 16711680, 10526975, 10987431, 31744, 16777215, 10791096,
 				9923917, 7368816, 4210943, 9402184, 16776437, 14188339, 11685080, 6724056, 15066419, 8375321, 15892389,
 				5000268, 10066329, 5013401, 8339378, 3361970, 6704179, 6717235, 10040115, 1644825, 16445005, 6085589,
 				4882687, 55610, 8476209, 7340544, 13742497, 10441252, 9787244, 7367818, 12223780, 6780213, 10505550,
 				3746083, 8874850, 5725276, 8014168, 4996700, 4993571, 5001770, 9321518, 2430480, 12398641, 9715553,
-				6035741, 1474182, 3837580, 5647422, 1356933 };
+				6035741, 1474182, 3837580, 5647422, 1356933, 6579300, 14200723, 8365974 };
 		// Weirdly, the colors are slightly different between the wiki, the
 		// output vanilla code creates and the game. Whatever.
 
@@ -444,6 +445,10 @@ public class MapConverter {
 		File file = new File(path);
 		try {
 			img = ImageIO.read(file);
+			if (img == null) {
+				System.out.println("Failed to read image. Try with a different image/image format.");
+				System.exit(0);
+			}
 		} catch (IOException e) {
 			System.out.println("Something went wrong:");
 			e.printStackTrace();
